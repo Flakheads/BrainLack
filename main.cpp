300894f8561bbe345355d1ae63a280de71155a3a
1000000000 <lthyde@gmail.com>
2017-06-25 10:00:02 -0400

    Add comments and more detailed error messages

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <limits>
 #include <stack>
 
 #include "program_tree.h"
@@ -9,6 +10,14 @@
 /*std::ostream& print_error(char* prog_name) {
 	return std::cerr
 */
+
+struct Character {
+	unsigned character, line;
+	char brace;
+
+	Character(unsigned the_char, unsigned the_line, char the_brace)
+		: character(the_char), line(the_line), brace(the_brace) {}
+};
 
 bool is_open_brace(char c) {
 	return c == '(' || c == '[' || c == '{' || c == '<';
@@ -36,27 +45,35 @@ int main(int argc, char* argv[]) {
 		return errno;
 	}
 	bool just_opened = false;
-	unsigned char_num = 0;
+	unsigned char_num = 0, line_num = 1;
 	char c;
-	std::stack<std::pair<unsigned,char> > braces;
+	std::stack<Character> braces;
 	while (src_file >> c) {
 		++char_num;
-		if (is_open_brace(c)) {
+		if (c == '#' || c == '\n') {
+			if (c == '#') {
+				src_file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			}
+			char_num = 0;
+			++line_num;
+		} else if (is_open_brace(c)) {
 			just_opened = true;
-			braces.push(std::make_pair(char_num,c));
+			braces.push(Character(char_num, line_num, c));
 		} else if (is_close_brace(c)) {
 			if (braces.empty()) {
-				std::cerr << argv[0] << ": unmatched " << c << " at " << char_num << std::endl;
+				std::cerr << argv[0] << ": unmatched " << c << " at line " << line_num << ", char " << char_num << std::endl;
 				break;
-			} else if (!is_matching(braces.top().second, c)) {
-				std::cerr << argv[0] << ": mismatched " << braces.top().second << " at character " << braces.top().first
-					<< " and " << c << " at character " << char_num << std::endl;
+			} else if (!is_matching(braces.top().brace, c)) {
+				std::cerr << argv[0] << ": mismatched " << braces.top().brace << " at line " << braces.top().line << ", char "
+					<< braces.top().character << " and " << c << " at line " << line_num << ", char " << char_num << std::endl;
 				break;
 			}
 			if (just_opened) {
-				std::cout << "Found " << braces.top().second << c << " nilad at " << braces.top().first << "-" << char_num << std::endl;
+				std::cout << "Found " << braces.top().brace << c << " nilad at " << braces.top().line
+					<< ':' << braces.top().character << '-' << line_num << ':' << char_num << std::endl;
 			} else {
-				std::cout << "Found " << braces.top().second << "..." << c << " monad at " << braces.top().first << "-" << char_num << std::endl;
+				std::cout << "Found " << braces.top().brace << c << " monad at " << braces.top().line
+					<< ':' << braces.top().character << '-' << line_num << ':' << char_num << std::endl;
 			}
 			braces.pop();
 			just_opened = false;
@@ -71,7 +88,8 @@ int main(int argc, char* argv[]) {
 	} else {
 		while (!braces.empty()) {
 			the_err = 1;
-			std::cerr << argv[0] << ": unmatched " << braces.top().second << " at character " << braces.top().first << std::endl;
+			std::cerr << argv[0] << ": unmatched " << braces.top().brace << " at line "
+				<< braces.top().line << ", char " << braces.top().character << std::endl;
 			braces.pop();
 		}
 	}
